54c04d65fefdf9e393fa0ef15740712cc0bc4807
link_er <link_er@bk.ru>
2016-06-06 20:24:07 +0200

    my data

--- assignment4/ssl_suites_hyst.py
+++ assignment4/ssl_suites_hyst.py
@@ -2,44 +2,17 @@ import matplotlib.pyplot as plt
 from collections import Counter
 
 suites = [line.rstrip('\n') for line in open('suites_openssl.txt')]
-usage = [line.rstrip('\n') for line in open('used_suites1.txt')]
+usage = [line.rstrip('\n') for line in open('used_suites.txt')]
 
 cntr = Counter(usage)
 
+plt.rcParams.update({'font.size': 8})
 plt.figure()
 ax = plt.subplot(111)
-ax.bar(range(0,25), cntr.values()[0:25])
-ax.set_xticks(map(lambda x: x + 0.5, range(0,25)))
-ax.set_xticklabels(cntr.keys()[0:25], rotation=90)
-ax.set_ylabel('Number of sites')
-
-plt.figure()
-ax = plt.subplot(111)
-ax.bar(range(0,25), cntr.values()[25:50])
-ax.set_xticks(map(lambda x: x + 0.5, range(0,25)))
-ax.set_xticklabels(cntr.keys()[25:50], rotation=90)
-ax.set_ylabel('Number of sites')
-
-plt.figure()
-ax = plt.subplot(111)
-ax.bar(range(0,25), cntr.values()[50:75])
-ax.set_xticks(map(lambda x: x + 0.5, range(0,25)))
-ax.set_xticklabels(cntr.keys()[50:75], rotation=90)
-ax.set_ylabel('Number of sites')
-
-plt.figure()
-ax = plt.subplot(111)
-ax.bar(range(0,25), cntr.values()[75:100])
-ax.set_xticks(map(lambda x: x + 0.5, range(0,25)))
-ax.set_xticklabels(cntr.keys()[75:100], rotation=90)
-ax.set_ylabel('Number of sites')
-
-all_used = len(cntr.values())
-plt.figure()
-ax = plt.subplot(111)
-ax.bar(range(0,all_used-100), cntr.values()[100:all_used])
-ax.set_xticks(map(lambda x: x + 0.5, range(0,all_used-100)))
-ax.set_xticklabels(cntr.keys()[100:all_used], rotation=90)
+quant = len(cntr.values())
+ax.bar(range(0,quant), cntr.values())
+ax.set_xticks(map(lambda x: x + 0.5, range(0,quant)))
+ax.set_xticklabels(cntr.keys(), rotation=90)
 ax.set_ylabel('Number of sites')
 
 plt.show()

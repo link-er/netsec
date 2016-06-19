ad7e037e81656302cd5daaea7bbf61b04e03a4c0
link_er <link_er@bk.ru>
2016-06-19 22:22:03 +0200

    update written part

--- assignment5/break_radius.py
+++ assignment5/break_radius.py
@@ -1,5 +1,4 @@
 import hashlib
-import re
 
 def xorWord(s0, s1):
   l = [ chr ( ord (a) ^ ord (b) ) for a,b in zip (s0, s1) ]
@@ -11,12 +10,5 @@ encr_pwd = '15efb479ae5bb57fdda1f2d011a51e18'
 with open('rfc7511.txt','r') as f:
   for line in f:
     for word in line.split():
-      clean_word = re.sub('[!@#$().,?\[\]]"\'', '', word)
-      if xorWord('0000000000000000', hashlib.md5(clean_word + authent.decode('hex')).digest()) == encr_pwd.decode('hex') :
-        print clean_word
       if xorWord('0000000000000000', hashlib.md5(word + authent.decode('hex')).digest()) == encr_pwd.decode('hex') :
-        print word
-      if xorWord('0000000000000000', hashlib.md5(clean_word + authent.decode('hex')).hexdigest()) == encr_pwd.decode('hex') :
-        print clean_word
-      if xorWord('0000000000000000', hashlib.md5(word + authent.decode('hex')).hexdigest()) == encr_pwd.decode('hex') :
         print word
